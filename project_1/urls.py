5cc5bfdec8692fdeecc245a9a492fe5dafd978f5
Gidituri Jayaram <140802567+Jairam2201@users.noreply.github.com>
2025-07-16 10:32:09 +0530

    Update urls.py

--- project_1/urls.py
+++ project_1/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import forgot_password,password_reset,view,time_table,signup,login,user_login,user_signup,date,edit,insert,save_work,sample,check,edit_work,delete,del_date
-from app_1.views import hii
+
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("register",signup,name="signup"),
+    path("register/",signup,name="signup"),
+    path('', RedirectView.as_view(url='/register/', permanent=False)),
     path("user_signup",user_signup,name="user_signup"),
     path("login",login,name="login"),
     path("user_login",user_login,name="user_login"),
@@ -41,7 +42,7 @@ urlpatterns = [
     path("view",view,name="view"),
     path("password_reset",password_reset,name="password_reset"),
     path("forgot_password",forgot_password,name="forgot_password"),
-    path("hello",hii,name="hii"),
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
